383325ad37a03f14cf73c94d7eec12ef05b3e392
Rudzainy <rudzainy@gmail.com>
2022-07-01 21:04:27 +0800

    Minor updates.
    - Replace (almost) all favicons
    - Update repo link
    - Update link in footer

--- components/footer.js
+++ components/footer.js
@@ -11,11 +11,11 @@ export default function Footer() {
             [🚧 Contents placeholder 🚧]
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <Link href="/">
+            <Link href="http://bincang.hoojah.my">
               <a
                 className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
               >
-                🏠 Home
+                Hoojah!
               </a>
             </Link>
             <Link href="/portfolio">
