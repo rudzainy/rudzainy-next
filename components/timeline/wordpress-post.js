11436ad3d1b24ef96043052f3f2a5b16a187a395
Rudzainy <rudzainy@gmail.com>
2022-07-01 20:11:02 +0800

    Reduce size of wordpress card for large screen..

--- components/timeline/wordpress-post.js
+++ components/timeline/wordpress-post.js
@@ -10,11 +10,25 @@ export default function WordpressPost({
   excerpt 
 }) {
   return (                
-    <li className="mb-10 ml-6">
+    <li className="mb-10 ml-6 w-fit md:w-10/12 lg:w-1/2">
       <span className="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white  ">
         <svg className="w-3 h-3 text-blue-600 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd"></path></svg>
       </span>
-      <Date dateString={date} dateClass="block pt-1 mb-2 text-sm font-normal leading-none text-gray-400" />    
+      <Date dateString={date} dateClass="block pt-1 mb-2 text-sm font-normal leading-none text-gray-400" />
+{/*       
+      <Link href={`/posts/${slug}`}>
+        <a className="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100">
+          {coverImage && (
+            // <img class="object-cover rounded-t-lg md:h-80 md:w-48 md:rounded-none md:rounded-l-lg" src={coverImage?.node.sourceUrl} alt=""></img>
+            <div class="bg-no-repeat bg-left-top h-80 w-48 rounded-none rounded-l-lg" style={{ backgroundImage: `url(${coverImage?.node.sourceUrl})` }}></div>
+          )}
+          <div className="flex flex-col justify-between p-4 leading-normal">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{title}</h5>
+            <p className="mb-3 font-normal text-gray-700" dangerouslySetInnerHTML={{ __html: excerpt }}></p>
+          </div>
+        </a>    
+      </Link> */}
+
       <div className="bg-white rounded-lg border border-gray-200 shadow-md">
         {coverImage && (
           <CoverImage title={title} coverImage={coverImage} slug={slug} />
