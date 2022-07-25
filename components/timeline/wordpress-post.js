8de784938d095f86ae173b9712a3b166eb11b40c
Rudzainy <rudzainy@gmail.com>
2022-07-25 10:19:58 +0800

    Minor fixes.
    - fix toggle menu alignments 
    - temporarily link out to WordPress site until post page is styled

--- components/timeline/wordpress-post.js
+++ components/timeline/wordpress-post.js
@@ -1,13 +1,13 @@
 import CoverImage from '../cover-image'
 import Date from '../date'
-import Link from 'next/link';
 
 export default function WordpressPost({ 
   title, 
   coverImage, 
   date, 
   slug, 
-  excerpt 
+  excerpt,
+  link
 }) {
   return (                
     <li className="mb-10 ml-6 w-fit md:w-10/12 lg:w-1/2">
@@ -37,12 +37,10 @@ export default function WordpressPost({
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{title}</h5>
           <time className="block mb-2 text-sm font-normal leading-none text-gray-400">{date}</time>
           {/* <p className="mb-3 font-normal text-gray-700" dangerouslySetInnerHTML={{ __html: excerpt }} /> */}
-          <Link href={`/posts/${slug}`}>
-            <a className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-              Read more
-              <svg className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-            </a>
-          </Link>
+          <a target="_blank" href={link} rel="noopener noreferrer" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+            Read more
+            <svg className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+          </a>
         </div>
       </div>
     </li>
--- pages/index.js
+++ pages/index.js
@@ -30,6 +30,7 @@ export default function Index({ sortedAllEvents, preview }) {
               date={event.date}
               slug={event.slug}
               excerpt={event.excerpt}
+              link={event.link}
             />
         )
       case 'workplace': 
@@ -66,35 +67,35 @@ export default function Index({ sortedAllEvents, preview }) {
         <Intro title={NAME} />
       </Container>
       <Container>
-        <div className="flex justify-center md:justify-end mb-16">
+        <div className="flex flex-wrap sm:justify-center md:justify-end mb-16">
           <div className="text-md text-gray-500 hidden md:block">Show/hide</div>
-          <label htmlFor="wordpress-toggle" className="ml-3 inline-flex relative items-center cursor-pointer">
+          <label htmlFor="wordpress-toggle" className="m-3 inline-flex relative items-center cursor-pointer">
             <input type="checkbox" value="" id="wordpress-toggle" className="sr-only peer" checked={showWordpressPosts} 
               onChange={(e) => setShowWordpressPosts(!showWordpressPosts)}
             />
             <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
             <span className="ml-3 text-sm font-medium text-gray-900">Blog posts</span>
           </label>
-          <label htmlFor="workplaces-toggle" className="ml-3 inline-flex relative items-center cursor-pointer">
+          <label htmlFor="workplaces-toggle" className="m-3 inline-flex relative items-center cursor-pointer">
             <input type="checkbox" value="" id="workplaces-toggle" className="sr-only peer" checked={showWorkplaces} 
               onChange={(e) => setShowWorkplaces(!showWorkplaces)}
             />
             <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
             <span className="ml-3 text-sm font-medium text-gray-900">Workplaces</span>
           </label>
-          <label htmlFor="twitter-toggle" className="ml-3 inline-flex relative items-center cursor-pointer">
+          <label htmlFor="twitter-toggle" className="m-3 inline-flex relative items-center cursor-pointer">
             <input type="checkbox" value="" id="twitter-toggle" className="sr-only peer" checked={showTwitterPosts} 
               onChange={(e) => setShowTwitterPosts(!showTwitterPosts)}
             />
             <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
             <span className="ml-3 text-sm font-medium text-gray-900">Twitter</span>
           </label>
-          {/* <label htmlFor="portfolio-toggle" className="ml-3 inline-flex relative items-center cursor-pointer">
+          {/* <label htmlFor="portfolio-toggle" className="m-3 inline-flex relative items-center cursor-pointer">
             <input type="checkbox" value="" id="portfolio-toggle" className="sr-only peer" />
             <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
             <span className="ml-3 text-sm font-medium text-gray-900">Portfolio</span>
           </label>
-          <label htmlFor="instagram-toggle" className="ml-3 inline-flex relative items-center cursor-pointer">
+          <label htmlFor="instagram-toggle" className="m-3 inline-flex relative items-center cursor-pointer">
             <input type="checkbox" value="" id="instagram-toggle" className="sr-only peer" />
             <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
             <span className="ml-3 text-sm font-medium text-gray-900">Instagram</span>
